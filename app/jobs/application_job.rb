dce029efbc56a4a797b7fceccb50c4a726bc8b47
Sit Min Htet <darknightmaster.smh@gmail.com>
2021-12-13 20:46:55 +0630

    finished first rails crud and user authority

--- app/jobs/application_job.rb
+++ app/jobs/application_job.rb
@@ -8,5 +8,4 @@ class ApplicationJob < ActiveJob::Base
   # Most jobs are safe to ignore if the underlying records are no longer available
   # discard_on ActiveJob::DeserializationError
 
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,5 @@ Rails.application.routes.draw do
   get 'contact', to: "home#contact", as: "contact1"
   get '/contact1', to: redirect("/contact")
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
 end
